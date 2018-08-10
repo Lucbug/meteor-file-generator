860294e9a3acca707c2d672082df6ebbee9a6f45
Lucas Bugnazet <lucas.bugnazet@gamestream.biz>
2018-08-10 11:20:16 +0200

    Initial commit

---
+++ spec/meteor-file-generator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MeteorFileGeneratorView from '../lib/meteor-file-generator-view';
+
+describe('MeteorFileGeneratorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
